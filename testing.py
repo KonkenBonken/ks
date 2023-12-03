24216046872af91c0e7e60acbc7684eb206e0fe9
KonkenBonken <konrad05pettersson@gmail.com>
2023-12-03 19:28:24 +0100

    add test feedback

--- testing.py
+++ testing.py
@@ -10,5 +10,15 @@ def run(script: str, *input: str):
     ).stdout.strip()
 
 
-assert run('let') == "{'A': True, 'B': 123, 'C': 12.3}"
-assert run('prompt', '1', '5') == "A: B: True\n5"
+def test(script: str, input: 'tuple[str]', expected: str):
+    output = run(script, *input)
+    if output == expected:
+        print(script, 'succeeded')
+    else:
+        print('\n{script} failed')
+        print({'expected': expected,
+               'recieved': output})
+
+
+test('let', '', "{'A': True, 'B': 123, 'C': 12.3}")
+test('prompt', ('1', '5'), "A: B: True\n5")
