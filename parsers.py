a8a8d7bcd7ceb5b63f27d3edf241545531cf974b
KonkenBonken <konrad05pettersson@gmail.com>
2023-12-04 22:49:40 +0100

    loop through operation sets

--- parsers.py
+++ parsers.py
@@ -21,31 +21,24 @@ def expressionparser(expr: str):
     if expr in varnames:
         return variables[expr]
 
-    if any(c in expr for c in '×÷'):
-        opindex = next(i for i, c in
-                       list(enumerate(expr))[::-1]
-                       if c in '×÷')
-        operator = expr[opindex]
+    for operators in ('×÷', '+-'):
+        if any(c in expr for c in operators):
+            opindex = next(i for i, c in
+                           list(enumerate(expr))[::-1]
+                           if c in operators)
+            operator = expr[opindex]
 
-        left, right = map(expressionparser, (expr[:opindex], expr[opindex+1:]))
+            left, right = map(expressionparser,
+                              (expr[:opindex], expr[opindex+1:]))
 
-        if operator == '×':
-            return left * right
-        elif operator == '÷':
-            return left / right
+            if operator == '×':
+                return left * right
+            elif operator == '÷':
+                return left / right
+            elif operator == '+':
+                return left + right
+            elif operator == '-':
+                return left - right
 
-    elif any(c in expr for c in '+-'):
-        opindex = next(i for i, c in
-                       list(enumerate(expr))[::-1]
-                       if c in '+-')
-        operator = expr[opindex]
-
-        left, right = map(expressionparser, (expr[:opindex], expr[opindex+1:]))
-
-        if operator == '+':
-            return left + right
-        elif operator == '-':
-            return left - right
-
-    elif expr[0] in datatypes:
+    if expr[0] in datatypes:
         return varparser(expr[0], expr[1:])
