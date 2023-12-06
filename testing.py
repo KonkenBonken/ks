5d0a69e45f45fb06cd19fc80b1dca003696ae9c6
KonkenBonken <konrad05pettersson@gmail.com>
2023-12-06 23:29:48 +0100

    made test input optional kwarg

--- testing.py
+++ testing.py
@@ -10,7 +10,8 @@ def run(script: str, *input: str):
     ).stdout.strip()
 
 
-def test(script: str, input: 'tuple[str]', expected: str):
+def test(script: str, expected: str, **kwargs: 'tuple[str]'):
+    input = kwargs.get('input', tuple())
     expected = expected.replace('⏎', '\n')
     output = run(script, *input)
     if output == expected:
@@ -21,8 +22,8 @@ def test(script: str, input: 'tuple[str]', expected: str):
                'recieved': output})
 
 
-test('let', '', 'A: True, B: 123, C: 12.3, D: 5, E: 6, F: 11')
-test('prompt', ('1', '5'), 'A: B: True⏎5')
-test('print', '', '5⏎3.1⏎2⏎False⏎2⏎False⏎2⏎5')
-test('operations', '', '5⏎8⏎8.2⏎10.2⏎2⏎1.8⏎6.8⏎15⏎7.5⏎2.0⏎7.5⏎4⏎407⏎9.0')
-test('goto', '', '1⏎3⏎True⏎True⏎4⏎4⏎4⏎4⏎3⏎3⏎3⏎2⏎2⏎1')
+test('let', 'A: True, B: 123, C: 12.3, D: 5, E: 6, F: 11')
+test('prompt', 'A: B: True⏎5', input=('1', '5'))
+test('print', '5⏎3.1⏎2⏎False⏎2⏎False⏎2⏎5')
+test('operations', '5⏎8⏎8.2⏎10.2⏎2⏎1.8⏎6.8⏎15⏎7.5⏎2.0⏎7.5⏎4⏎407⏎9.0')
+test('goto', '1⏎3⏎True⏎True⏎4⏎4⏎4⏎4⏎3⏎3⏎3⏎2⏎2⏎1')
